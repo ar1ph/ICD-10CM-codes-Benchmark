4de411ab0c6cee9419538ae5042768f84f0e6057
Tasnim1147 <tasayem1147@gmail.com>
2023-06-13 01:00:34 -0700

    Modularization and added the all ICD-10 CM codes in assets

---
+++ lib/process_json.py
@@ -0,0 +1,43 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import TextLoader
+import json
+import os 
+
+# Gets the path of the src_file (.json) 
+# Return the contents in dictionary format
+def get_dict_from_json(src_file):
+    try:  
+        with open(file=src_file, mode='r') as contents: 
+            return json.load(contents)
+    except Exception as e:
+        print('Error occured while reading config file: ' + str(e))
+        return None
+    
+# Gets the path of the dest_file (.json) and dictionary
+# Writes the contents of the dictionary in that file
+def store_dict_in_json(dest_file, contents):
+    try:
+        with open(file=dest_file, mode='w') as json_file:
+            json.dump(contents, json_file)
+            return True
+    except Exception as e:
+        print('Error occured while reading config file: ' + str(e))
+        return False
+    
+
+
+def main():
+    d = {"Hello": "World",
+         "Phone": 123}
+    
+    store_dict_in_json(dest_file='test.json', contents=d)
+    d2 = get_dict_from_json("test.json")
+    for k in d:
+        assert(k in d2 and d[k] == d2[k])
+    
+    print("Test passed")
+
+
+if __name__ == "__main__":
+    main()
