45c57aae740a2f3a2e0bdb41de68810d4e2e8c2e
Tasnim1147 <tsayem1147@gmail.com>
2023-06-13 11:39:33 +0300

    Created the get_all_codes in process_codes.py

--- lib/process_codes.py
+++ lib/process_codes.py
@@ -1,8 +1,7 @@
 import csv
 import os
-
-# Function for getting all the codes
-# Function for returning a dictionary where keys are codes and values are rest of the info in list
+ 
+# Function for returning a dictionary where keys are whole codes and values are rest of the info in list
 # Function for getting the rows containing a code
 # Function for getting medical condition given a code
 # Function for getting codes based on the provided diseas
@@ -10,18 +9,30 @@ import os
 CODE_DIRECTORY = os.path.join(os.path.abspath(os.pardir), "assets")
 CODE_FILE = os.path.join(CODE_DIRECTORY, 'codes.csv')
 
-# Returns list of all the codes
-def get_all_codes():
-    try:
-        with open(file=CODE_FILE, mode='r') as csvfile:
-            all_rows = csv.reader(csvfile, delimiter=',')
-            for row in all_rows[:2]:
-                print(row)
+# Row format = [Partial code, digit, Full code, desc, desc, disease]
 
+# Returns list of all the codes related to a disease()
+def get_all_codes(disease=""):
+    try:
+        all_codes = []
+        disease = disease.lower()
+        with open(file=CODE_FILE, mode='r') as csvfile:
+            all_rows = csv.reader(csvfile, delimiter=',') 
+            count = 0
+            for row in all_rows:
+                if disease != "":
+                    if row[-1].lower() == disease: all_codes.append(row[0])
+                else:
+                    all_codes.append(row[2])
+        return all_codes
     except Exception as e:
         print("Error occured while accessing codes: " + str(e))
         return None
     
 
 def main():
-    
+    print(len(get_all_codes()))
+
+
+if __name__ == "__main__":
+    main()
