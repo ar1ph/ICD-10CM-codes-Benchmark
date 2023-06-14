2eb6ba43264acb98d4e039b179e4d15467364596
Tasnim1147 <tasayem1147@gmail.com>
2023-06-14 00:13:33 -0700

    Resolved the issue with getting the diseases from codes.csv

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,10 +1,15 @@
 import os
 import csv
+import sys
 
 BENCHMARK_DIRECTORY = os.path.join(os.path.abspath(os.pardir), "benchmark")
 BENCHMARK_FILE = os.path.join(BENCHMARK_DIRECTORY, "benchmark.csv") 
 ASSETS_DIRECTORY = os.path.join(os.path.abspath(os.pardir), "assets")
 QUERY_FILE = os.path.join(ASSETS_DIRECTORY, "Queries.txt")
+
+sys.path.append('..')
+from lib.process_json import *
+from lib.process_data import *
 
 # Gets a report dictionary and file path to a csv file
 # Inserts a row containing the report
@@ -35,6 +40,12 @@ def initialize_benchmark(file_path=BENCHMARK_FILE):
 
 def main():
     initialize_benchmark()
+    all_codes = get_all_codes()
+    # print(all_codes)
+    code_map = get_new_subset_of_codes(full_row=True)
+    # print(code_map)
+    all_disease = retrieve_all_diseases(code_map=code_map)
+    print(all_disease)
     report = {'Embedding Model': 'all_mini',
               'DB Type': 'Chroma',
               'Strategy': 'Cosine similarity',
