dead0d3af2dba359b97f97d1c46b52cafb68d20d
Tasnim1147 <tasayem1147@gmail.com>
2023-06-13 22:56:24 -0700

    Changed the loacation of Queries.txt

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -3,6 +3,8 @@ import csv
 
 BENCHMARK_DIRECTORY = os.path.join(os.path.abspath(os.pardir), "benchmark")
 BENCHMARK_FILE = os.path.join(BENCHMARK_DIRECTORY, "benchmark.csv") 
+ASSETS_DIRECTORY = os.path.join(os.path.abspath(os.pardir), "assets")
+QUERY_FILE = os.path.join(ASSETS_DIRECTORY, "Queries.txt")
 
 # Gets a report dictionary and file path to a csv file
 # Inserts a row containing the report
@@ -28,6 +30,8 @@ def initialize_benchmark(file_path=BENCHMARK_FILE):
     with open(file=file_path, mode='w') as fn:
         csv_writter = csv.writer(fn, delimiter=',', lineterminator='\n')
         csv_writter.writerow(row)
+
+# 
 
 def main():
     initialize_benchmark()
