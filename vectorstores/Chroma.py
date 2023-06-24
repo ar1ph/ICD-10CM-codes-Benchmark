c15629f5f5ba65f9a21838d7603d36b33c5b1775
Tasnim1147 <tsayem1147@gmail.com>
2023-06-24 19:31:19 +0300

    Created a new chroma class considering the embedding abstract class

--- vectorstores/Chroma.py
+++ vectorstores/Chroma.py
@@ -1,131 +1,138 @@
 
+import sys, os
+from typing import Any
+from base import BaseVectorstore
+from tqdm import tqdm
+from uuid import uuid1
 from chromadb import Client
 from chromadb.config import Settings
-import os
-from sentence_transformers import SentenceTransformer
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import TextLoader
-from uuid import uuid1
-from tqdm import tqdm
+sys.path.append('..')
+from embeddings.base import BaseEmbedding
+from embeddings.HuggingFaceEmbedding import HuggingFaceEmbedding
 
 
-class Chroma(object):
+
+class Chroma(BaseVectorstore):
+
+    SEARCH_STRATEGIES = {'ip',
+                         'cosine',
+                         'l2'}
 
     def __init__(self,
-                 embedding_model_name,
-                 strategy) -> None:
+                 embedding,
+                 strategy
+                 ) -> None:
         
         _DATABASE_DIRECTORY = os.path.join(os.path.abspath(os.pardir), 
                                            "database")
+        self.embedding = embedding
+        self.strategy = strategy
+        _emb_model_name = embedding.get_name()
         self.persist_directory = os.path.join(_DATABASE_DIRECTORY,
-                                              f"{embedding_model_name}__Chroma")
-        self.client = Client(Settings(
+                                              f"{_emb_model_name}__Chroma")
+        self._client = Client(Settings(
             chroma_db_impl="duckdb+parquet",
             persist_directory= self.persist_directory
         ))
+        self._add_collection()
+        super().__init__()
 
-        self.embedding_model = SentenceTransformer(embedding_model_name)
-        self.strategy = strategy
 
-        self._collection_name = f"chroma__collection"
-        # print(self._collection_name)
-        self.collection = self.client.get_or_create_collection(name=self._collection_name,
-                                                               embedding_function=self.embedding_model.encode
-                                                               )
-        # self.collection = self.client.get_collection(name=self._collection_name,
-        #                                                        embedding_function=self.embedding_model.encode,
-        #                                                        )
-        self.collection.modify(metadata={"hnsw:space":strategy})
+    def _collection_exist(self,
+                         name : str) -> bool:
+        try:
+            collection = self._client.get_collection(name)
+            return True
+        except:
+            return False
 
-    def _split_documents(self, data_directory):
-        splitter = RecursiveCharacterTextSplitter(chunk_size=750,
-                                                chunk_overlap=100)
-        file_paths = []
-        # print(data_directory)
-        # DATA_DIRECTORY = os.path.join(os.path.abspath(os.pardir), "data")
-        db_files = set(self.get_source_files())
-        for root, _, files in os.walk(data_directory):
-            for file in files:
-                if file not in db_files: file_paths.append(os.path.join(root, file))
-        # Modify here for parallelism or progress ase
-        if len(file_paths) > 0:
-            docs = TextLoader(file_path=file_paths[0], encoding='utf-8').load()
-            with tqdm(total=len(file_paths), desc='Loading documents', ncols=80) as pbar1:
-                for path in file_paths:
-                    docs.extend(TextLoader(file_path=path, encoding='utf-8').load())
-                    pbar1.update()
-            splitted_docs = splitter.split_documents(docs)
-            return splitted_docs
-    
-    def _initialize_contents(self, 
-                             docs):
-        metadatas = []
-        documents = []
-        with tqdm(total=len(docs), desc='Initializing page contents and metadatas', ncols=80) as pbar2:
-            for doc in docs:
-                documents.append(doc.page_content)
-                metadatas.append(doc.metadata)
-                pbar2.update()
-        ids = [str(uuid1()) for _ in range(len(documents))]
-        db_contents = {'ids': ids,
-                       'metadatas': metadatas,
-                       'documents': documents}
+
+
+    def __setattr__(self, 
+                    __name: str, 
+                    __value: Any
+                    ) -> None:
         
-        return db_contents
+        if __name == "embedding":
+            if not isinstance(__value, BaseEmbedding):
+                error_msg = "Embedding must be of BaseEmbedding type"
+                raise ValueError(error_msg)
+        elif __name == "strategy":
+            if not __value in Chroma.SEARCH_STRATEGIES:
+                error_msg = f"{__value} search strategy is not supported"
+                raise ValueError(error_msg)
+            
+        return super().__setattr__(__name, __value)
+    
+
+    def _add_collection(self):
+
+        name = 'chroma_collection'
+        metadata = {'hnsw:space':self.strategy,
+                    'hnsw:construction_ef':4096,
+                    'hnsw:search_ef':4096,
+                    'hnsw:M':100}
+        func = self.embedding.get_function()
+        if self._collection_exist(name): self._client.reset()
+        print(self._client.list_collections())
+        self._collection = self._client.create_collection(name,
+                                                          metadata,
+                                                          func)
+        
+    
 
     def add_data(self, 
-                 data_directory):
-
+                 data_directory: str) -> None:
+        
         try:
-            splitted_docs = self._split_documents(data_directory)
-            db_contents = self._initialize_contents(splitted_docs)
-            self.collection.add(**db_contents)
-            # print(db_contents)
-            self.client.persist()
+            docs = super().process_documents(data_directory=data_directory)
+            with tqdm(total=len(docs), 
+                      desc="Adding documents", 
+                      ncols=80) as pbar:
+                for doc in docs:
+                    # embedding = self.embedding.from_text(doc.page_content)
+                    self._collection.add(ids=[str(uuid1())],
+                                        # embeddings=[embedding],
+                                        metadatas=[doc.metadata],
+                                        documents=[doc.page_content]
+                                        )
+                    pbar.update()
+            self._client.persist()
             return True
         except Exception as e:
-            print("Error occured while adding docs: " + str(e))
+            print("Error occured while adding documents" + str(e))
             return False
         
-
     def query(self, 
-              query_text,
-              n_results):
+              query_text: str, 
+              n_results: int,
+              inclue: list[str]):
         if n_results == -1:
-            # print(f"n_results: {self.collection.count()}")
-            return self.collection.query(query_texts=query_text,
-                                         n_results=self.collection.count())
-        return self.collection.query(query_texts=query_text,
-                                     n_results=n_results)
+            return self._collection.query(query_texts=query_text,
+                                          n_results=self.get_max_n(),
+                                          include=inclue)
+        return self._collection.query(query_text=query_text,
+                                      n_results=n_results,
+                                      include=inclue)
+        
+    def get_available_strategies(self) -> list[str]:
+        return Chroma.SEARCH_STRATEGIES
     
-    def get_available_startegies(): return ['ip','cosine','l2']
-    # def get_available_startegies(): return ['cosine', 'ip']
+    def get_max_n(self) -> int:
+        return self._collection.count()
+
     
-    def get_strategy(self): return self.strategy
-
-    def count(self): return self.collection.count()
-
-    def get_source_files(self): 
-        metadatas = self.collection.get()['metadatas']
-        sources = []
-        for metadata in metadatas:
-            sources.append(os.path.basename(metadata['source']))
-        return sources
-
-    def reset(self): self.client.reset()
-
-
 
 
 def main():
-
-    chroma_obj = Chroma(embedding_model_name='all-MiniLM-L6-v2', strategy='cosine')
-    print(chroma_obj.count())
-    chroma_obj = Chroma(embedding_model_name='all-MiniLM-L6-v2', strategy='ip')
-    print(chroma_obj.count())
-    chroma_obj = Chroma(embedding_model_name='all-MiniLM-L6-v2', strategy='l2')
-    print(chroma_obj.count())
+    embedding = HuggingFaceEmbedding(model_name='all-MiniLM-L6-v2')
+    chroma = Chroma(embedding=embedding,
+                    strategy='ip')
+    data_directory = os.path.join(os.path.abspath(os.curdir),
+                                  'data_temp')
+    chroma.add_data(data_directory=data_directory)
+    print(chroma.query(query_text='Describe the ICD-10 Code A01.2',
+                       n_results=-1))
 
 
 if __name__ == "__main__": main()
-
